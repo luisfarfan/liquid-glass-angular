9a4f295603f605c5e63e8a1102052e9a72a7d62e
Luis Eduardo Farfan Melgar <lucho.farfan9@gmail.com>
2026-04-19 22:42:13 -0500

    build(deps): remove animations and synchronize angular versions to 21.2.7

--- apps/playground/src/app/app.config.ts
+++ apps/playground/src/app/app.config.ts
@@ -3,7 +3,6 @@ import {
   provideBrowserGlobalErrorListeners,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideAnimations } from '@angular/platform-browser/animations';
 import { importProvidersFrom } from '@angular/core';
 import { DialogModule } from '@angular/cdk/dialog';
 import { appRoutes } from './app.routes';
@@ -75,7 +74,6 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(), 
     provideRouter(appRoutes),
-    provideAnimations(),
     importProvidersFrom(DialogModule),
     provideLGThemes({
       themes: customThemes,
